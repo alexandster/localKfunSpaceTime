34bf8107d12390be940225d3a4db1affcf5553f6
Alexander Hohl <34194710+alexandster@users.noreply.github.com>
2018-02-10 19:36:09 -0500

    Update results_collect.py

--- results_collect.py
+++ results_collect.py
@@ -1,10 +1,10 @@
 #import modules
 import os, numpy as np
-
+from datetime import datetime
 
 #read observed local k function
-headArr = np.loadtxt("outputs/obs/localk.txt",delimiter=" ",usecols = (0,1,2,3)) 
-obsArr = np.delete(np.loadtxt("outputs/obs/localk.txt",delimiter=" "),(0,1,2,3),1)
+headArr = np.loadtxt("outputs/obs/localk.txt",delimiter=",",usecols = (0,1,2,3))
+obsArr = np.delete(np.loadtxt("outputs/obs/localk.txt",delimiter=","),(0,1,2,3),1)
 
 #initialize zero array and infinity array
 maxArr = np.zeros(obsArr.shape)
@@ -12,9 +12,10 @@ minArr = np.full(obsArr.shape, np.inf)
 
 #find simulation envelopes
 i = 1
+startTime = datetime.now()
 while i<=len(os.listdir('outputs/sim')):
 
-    simArr = np.delete(np.loadtxt("outputs/sim/localk" + str(i) + ".txt",delimiter=" "),(0,1,2,3),1)
+    simArr = np.delete(np.loadtxt("outputs/sim/localk" + str(i) + ".txt",delimiter=","),(0,1,2,3),1)
 
     maxArr1 = np.amax(np.stack([maxArr, simArr]),axis=0)
     maxArr = maxArr1
@@ -25,5 +26,9 @@ while i<=len(os.listdir('outputs/sim')):
     i += 1
 
 #compute differences between observed local k function and simulation envelopes
-np.savetxt('outputs/diff/obs_max.txt', np.concatenate((headArr, np.subtract(obsArr,maxArr)), axis=1))	#difference between observed and upper envelope
-np.savetxt('outputs/diff/obs_min.txt', np.concatenate((headArr, np.subtract(obsArr,minArr)), axis=1))   #difference between observed and lower envelope
+np.savetxt('outputs/diff/obs_max.txt', np.concatenate((headArr, np.subtract(obsArr,maxArr)), axis=1),fmt='%1.2f',delimiter=',')	#difference between observed and upper envelope
+np.savetxt('outputs/diff/obs_min.txt', np.concatenate((headArr, np.subtract(obsArr,minArr)), axis=1),fmt='%1.2f',delimiter=',')   #difference between observed and lower envelope
+
+endTime = datetime.now()
+runTime = endTime - startTime
+print("execution time: ", runTime)
